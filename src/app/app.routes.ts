a6f4166f3268777d10ee3d84644cfef1dcc3761b
Dominic Järmann <dominic.jaermann@golog.ch>
2026-04-19 21:13:10 +0200

    feat: ugly login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from "@angular/router";
+import {Login} from "./features/login/login";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'authentication', component: Login},
+    {path: '', redirectTo: 'authentication', pathMatch: 'full'},
+];
--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-
+import {Component, signal} from '@angular/core';
+import { createClient } from '@workos-inc/authkit-js';
+import {Card} from "primeng/card";
+import {DatePicker} from "primeng/datepicker";
+import {InputText} from "primeng/inputtext";
+import {PasswordDirective} from "primeng/password";
+import {Button} from "primeng/button";
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    Card,
+    DatePicker,
+    InputText,
+    PasswordDirective,
+    Button
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  protected isLoginMode = signal(false);
+
 
 }
