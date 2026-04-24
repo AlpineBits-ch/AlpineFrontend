8d521f095cc55f67cb5d61ae1ebc28696b987678
Dominic Järmann <dominic.jaermann@golog.ch>
2026-04-24 09:56:35 +0200

    chore: fix subscribe to login

--- src/app/features/overview/overview.ts
+++ src/app/features/overview/overview.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AuthService} from "../../services/auth-service";
+import {Router} from "@angular/router";
+import {Button} from "primeng/button";
 
 @Component({
   selector: 'app-overview',
-  imports: [],
+  imports: [
+    Button
+  ],
   templateUrl: './overview.html',
   styleUrl: './overview.css',
 })
 export class Overview {
+  protected  authService = inject(AuthService);
 
+  protected router = inject(Router);
+  public logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/authentication']);
+  }
 }
