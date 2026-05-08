7281d02d5b1f3e8863ce72361a04eb457fc19444
Dominic Järmann <dominic.jaermann@golog.ch>
2026-05-08 13:15:22 +0200

    chore: remove branching where no sensos

--- src/app/components/typing-dots/typing-dots.component.ts
+++ src/app/components/typing-dots/typing-dots.component.ts
@@ -1,25 +1,14 @@
-import { Component, inject } from '@angular/core';
-import { PlatformService } from '../../services/platform.service';
+import { Component } from '@angular/core';
 
 /** Three animated dots indicating that someone is typing. */
 @Component({
   selector: 'app-typing-dots',
   template: `
-    @if (platformService.isMobile) {
-      <span class="flex gap-0.5 items-end shrink-0">
-        <span class="typing-dot"></span>
-        <span class="typing-dot"></span>
-        <span class="typing-dot"></span>
-      </span>
-    } @else {
-      <span class="flex gap-0.5 items-end shrink-0">
-        <span class="typing-dot"></span>
-        <span class="typing-dot"></span>
-        <span class="typing-dot"></span>
-      </span>
-    }
+    <span class="flex gap-0.5 items-end shrink-0">
+      <span class="typing-dot"></span>
+      <span class="typing-dot"></span>
+      <span class="typing-dot"></span>
+    </span>
   `,
 })
-export class TypingDotsComponent {
-  public platformService = inject(PlatformService);
-}
+export class TypingDotsComponent {}
