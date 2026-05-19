18c64cae05ee5d73828e1e5fb12cfc850c83d611
Dominic Järmann <dominic.jaermann@golog.ch>
2026-05-19 18:16:04 +0200

    feat: update version

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -4,13 +4,14 @@ import {PasswordDirective} from "primeng/password";
 import {Button} from "primeng/button";
 import {AuthService} from "../../services/auth.service";
 import {catchError, EMPTY, tap} from "rxjs";
-import {form, FormField} from "@angular/forms/signals";
+import {email, form, FormField, PathKind, pattern, required, SchemaPath} from "@angular/forms/signals";
 import {Router} from "@angular/router";
 import {NgClass} from "@angular/common";
 import {UserSettingsService} from "../../services/user-settings.service";
 import {ToastService} from "../../services/toast.service";
 import {TranslateModule} from '@ngx-translate/core';
 import {EmailVerificationService} from '../../services/email-verification.service';
+import Child = PathKind.Child;
 
 
 interface LoginModel {
@@ -52,8 +53,33 @@ export class Login {
         birthdate: ''
     });
     protected passwordMismatch = signal(false);
-    protected loginForm = form(this.loginModel);
-    protected registerForm = form(this.registerModel);
+    protected loginForm = form(this.loginModel, (schema) => {
+
+    });
+    protected registerForm = form(this.registerModel, (schema) => {
+        required(schema.birthdate, {
+            message: 'Birthdate is required.',
+        })
+        required(schema.email, {
+            message: 'Email is required.',
+        })
+        required(schema.password, {
+            message: 'Password is required.',
+        })
+        required(schema.confirmPassword, {
+            message: 'Confirm password is required.',
+        })
+
+        email(schema.email, {
+            message: 'Please enter a valid email address.',
+        })
+
+        pattern(
+            schema.birthdate,
+            /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.(19|20)\d\d$/,
+            { message: 'Please enter a valid date in dd.mm.yyyy format.' }
+        );
+    });
     private userSettings = inject(UserSettingsService);
     private toast = inject(ToastService);
     private emailVerification = inject(EmailVerificationService);
@@ -93,17 +119,36 @@ export class Login {
     }
 
     protected register(): void {
+        this.registerForm().markAsTouched();
+        this.registerForm().markAsDirty();
+
+        this.registerForm.birthdate().markAsTouched();
+        this.registerForm.email().markAsTouched();
+        this.registerForm.password().markAsTouched();
+        this.registerForm.confirmPassword().markAsTouched();
+        this.registerForm.username().markAsTouched();
+
+        this.registerForm.birthdate().markAsDirty();
+        this.registerForm.email().markAsDirty();
+        this.registerForm.password().markAsDirty();
+        this.registerForm.confirmPassword().markAsDirty();
+        this.registerForm.username().markAsDirty();
+
         const model = this.registerModel();
         if (model.password !== model.confirmPassword) {
             this.passwordMismatch.set(true);
             return;
         }
+
+
+        if(!this.registerForm().valid()) return;
+
         this.passwordMismatch.set(false);
         this.authService.register(
-            this.registerModel().email,
-            this.registerModel().username,
-            this.registerModel().password,
-            this.parseBirthdate(this.registerModel().birthdate)
+            this.registerModel().email!,
+            this.registerModel().username!,
+            this.registerModel().password!,
+            this.parseBirthdate(this.registerModel().birthdate!)
         ).pipe(
             tap(() => {
                 this.toast.success('Account created!', {detail: 'Welcome to Alpine. You can now sign in.'});
